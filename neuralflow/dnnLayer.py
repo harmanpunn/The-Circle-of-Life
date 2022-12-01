24de4a71426978077aa1765c3548200380492547
aa20475 <aa20475@gmail.com>
2022-12-01 17:36:39 -0500

    Changes forward pass && comments in Value iteration

--- neuralflow/dnnLayer.py
+++ neuralflow/dnnLayer.py
@@ -10,16 +10,51 @@ class DNNLayer:
         self.output = output
         self.batchSize = batchSize
 
-        self.weights = np.random.rand(self.output, self.input) if weights == None else weights
-        self.bias = np.random.rand(self.output, self.batchSize) if bias == None else bias
+        self.lastLayer = False
 
-    def forward(self, x):
-        x = np.array(x)
-        if x.transpose().shape!=(self.input,self.batchSize):
-            raise ValueError("Expected input of size",(self.input,self.batchSize)," got ",x.transpose().shape," instead")
-        
-        return self.activationFunction(np.add(np.matmul(self.weights, x.transpose()),self.bias)).transpose()
-        
-# layer = DNNLayer(2,3)
+        self.weights = np.random.rand(self.output, self.input+1) if weights == None else weights
 
-# print(layer.forward([[1,2]]))
+        self.memory = {
+            "in" : None,
+            "out": None
+        }
+        self.memoryFlag = False
+
+    def forward(self, x1):
+        x = np.array(x1)
+        
+        if x.shape[1]!=self.input:
+            raise ValueError("Invalid input shape, expected "+str(self.input)+" got "+str(x.shape[1]))
+        
+        x = x.transpose()
+        x = np.append([np.ones(x.shape[1])],x,axis=0)
+        
+        self.memory["in"] = x
+        out =  np.matmul(self.weights, x)
+        self.memory["out"] = out
+        self.memoryFlag = True
+
+        return self.activationFunction(self.memory["out"]).transpose()
+
+    def backward(self, x):
+        if not self.memoryFlag:
+            raise MemoryError("Run a proper forward pass before back prop")
+        
+        print(self.memory["in"].shape)
+        print(self.memory["out"].shape)
+        gradient = np.matmul(self.activationGradient(self.memory["out"]),self.memory["in"].transpose())
+        print(gradient)
+        print(gradient.shape)
+        print(self.weights.shape)
+        self.memoryFlag = False
+
+        # calculate gradient
+        # if self.lastLayer:
+
+            
+
+    
+layer = DNNLayer(2,10,2)
+
+print(layer.forward([[1,2],[2,3]]))
+print(layer.backward([1,2,3]))
